12f9a31d66857dbbd62e86ba7a01a6213469b5d7
José Pablo Santillana <jos_pab_997@hotmail.com>
2026-01-14 11:41:29 -0600

    added docker

--- servicepart/src/main/java/com/mianki/servicio/servicepart/security/SecurityConfig.java
+++ servicepart/src/main/java/com/mianki/servicio/servicepart/security/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
 
     @Bean
     PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(12);
     }
 
     @Bean
